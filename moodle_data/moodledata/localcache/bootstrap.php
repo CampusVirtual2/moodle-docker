0616821aefc2f2db4ffa44b815d0080a8a17af63
fabiotabosa <fabio.tabosa@gmail.com>
2025-10-31 01:21:25 +0000

    limpeza do moodledata duplicado

--- moodle_data/moodledata/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'kiUDinQlePjjVhpOGhTjbTNA8ZaIEiLAhomol.moodle.agu.redes.unb.br';
-$CFG->bootstraphash = '076f941b87ee1d1829e6c08387b1082e';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
